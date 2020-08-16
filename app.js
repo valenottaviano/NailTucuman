01b7832f90daee6a7ebca1869688f4ffdf0dfdb5
valenottaviano <valentinottaviano@gmail.com>
2020-08-16 13:01:18 -0300

    burger menu modified

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const burger = document.querySelector(".burger");
+const lines = document.querySelectorAll(".burger div");
 const navLinks = document.querySelector(".nav-links");
 const navLink = document.querySelectorAll(".nav-links li");
 
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+  navLink.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `linksFade 0.3s ease-in forwards ${
+        index / 5 + 0.5
+      }s`;
+    }
+  });
+  lines.forEach((line) => {
+    line.classList.toggle("toggle");
+  });
 });
 
 navLink.forEach((e) => {
   e.addEventListener("click", function () {
     if (navLinks.classList.contains("active")) {
+      navLink.forEach((link) => {
+        link.style.animation = "";
+      });
       navLinks.classList.remove("active");
     }
   });
